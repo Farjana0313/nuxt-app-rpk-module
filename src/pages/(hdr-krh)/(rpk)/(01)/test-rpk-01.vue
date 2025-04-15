f3c648831477c0695b075b7dac23b20c1bfe960e
Farjana0313 <farjanaakter178@gmail.com>
2025-04-15 22:42:38 +0600

    shift component rpk

--- src/pages/(hdr-krh)/(rpk)/(01)/test-rpk-01.vue
+++ src/pages/(hdr-krh)/(rpk)/(01)/test-rpk-01.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="container">
-    <h1>Test 02</h1>
+    <!-- <h1>Test 02</h1> -->
+     <!--- Results Table End -->
+  <!-- Owner Search -->
+  <CompetencyOwnerSearch/>
+  <!-- Owner Search End-->
+  <!-- List of Competency Owners -->
+  <ListOfCompetencyOwners />
+  <!-- List of Competency Owners End -->
+  <!-- Attendance Records Table -->
+  <AttendanceInformationSearch/>
+  <!-- Attendance Records Table End -->
+  <SearchThroughAdHocIntegration />
   </div>
 </template>
+<script setup lang="ts">
+import ListOfCompetencyOwners from "./components/feature/tables/ListOfCompetencyOwners.vue";
+import AttendanceInformationSearch from "./components/feature/records/AttendanceInformationSearch.vue";
+import SearchThroughAdHocIntegration from "./components/feature/records/SearchThroughAdHocIntegration.vue";
+import CompetencyOwnerSearch from "./components/feature/records/CompetencyOwnerSearch.vue";
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,30 +4,16 @@
   <!-- Page Header End -->
 
   <!-- Search Panel -->
-  <!-- <SearchPanel /> -->
+  <SearchPanel />
   <!-- Search Panel End -->
 
   <!-- Results Table -->
-  <!-- <Results /> -->
+  <Results />
   <!--- Results Table End -->
-  <!-- Owner Search -->
-  <CompetencyOwnerSearch />
-  <!-- Owner Search End-->
-  <!-- List of Competency Owners -->
-  <ListOfCompetencyOwners />
-  <!-- List of Competency Owners End -->
-  <!-- Attendance Records Table -->
-  <AttendanceInformationSearch/>
-  <!-- Attendance Records Table End -->
-  <SearchThroughAdHocIntegration />
 </template>
 
 <script setup lang="ts">
 import PageHeader from "@/components/common/PageHeader.vue";
 import SearchPanel from "@/components/feature/dashboard/SearchPanel.vue";
 import Results from "@/components/feature/dashboard/Results.vue";
-import CompetencyOwnerSearch from "./(hdr-krh)/(rpk)/(01)/components/feature/records/CompetencyOwnerSearch.vue";
-import ListOfCompetencyOwners from "./(hdr-krh)/(rpk)/(01)/components/feature/tables/ListOfCompetencyOwners.vue";
-import AttendanceInformationSearch from "./(hdr-krh)/(rpk)/(01)/components/feature/records/AttendanceInformationSearch.vue";
-import SearchThroughAdHocIntegration from "./(hdr-krh)/(rpk)/(01)/components/feature/records/SearchThroughAdHocIntegration.vue";
 </script>
